773e92bf4bb5cfbd5ee606d90537f03c5fb0069a
Suhas Aggarwal <suhasagg@gmail.com>
2023-01-26 00:31:55 +0530

    Update fastpowerset.go

--- fastpowerset.go
+++ fastpowerset.go
@@ -14,3 +14,7 @@ func subsets(set []int) [][]int {
 
     return result
 }
+
+//In this example, the powerSet function uses an iterative approach to generate the power set of the given set. It uses a for-loop to iterate through all possible subsets represented by a bit mask, and checks if the current subset includes the corresponding element of the set by using the bitwise AND operator. The function then appends the current subset to the result.
+//This approach is faster than the recursive approach because it avoids the overhead of creating and destroying multiple stack frames and it's also faster than the concurrent approach because it doesn't need to handle synchronization
+//Beats 100 percent soultions in leet code 0 ms runtime
